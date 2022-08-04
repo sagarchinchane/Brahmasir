6bde0c65008aee5ba5eb0d3038bf450b1d40624d
“Sagarchinchane <sagar.chinchane@kalpitatechnologies.com>
2022-08-04 14:30:55 +0530

    multilevel Inheritence

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Multilevel
+{
+    class GrandFather
+    {
+        public void Display()
+        {
+            Console.WriteLine("grandfather's car");
+        }
+    }
+    class Father : GrandFather
+    {
+        public void Disp()
+        {
+            Console.WriteLine("Father's bike");
+        }
+    }
+    class son : Father
+    {
+        public void Displ()
+        {
+            Console.WriteLine("son's cycle");
+            
+        }
+    }
+
+    class Multiple
+    {
+        static void Main(String[] args)
+        {
+            son s =new son();
+            s.Displ();
+            s.Disp();
+            s.Display();
+            
+
+        }
+    }
+}
+    
+
+   
+
